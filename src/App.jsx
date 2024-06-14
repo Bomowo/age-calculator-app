68ba711126a335c18f373b11788520a466b473d1
Bomowo <oeccc25@yahoo.com>
2024-06-14 21:51:06 +0300

    Make working input fields with restrictions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react"
+
 function App() {
+  let [[day, month, year], setDate] = useState([1,1,1])
+
+  function changeDay (e) {
+    console.log(e.target.value)
+    if((e.target.value <= 31 && e.target.value>=1)  || e.target.value === '') {
+      setDate((prevDate) => {
+        return [e.target.value, prevDate[1], prevDate[2]]
+      })
+    }
+  }
+
+  function changeMonth (e) {
+    if((e.target.value <= 12 && e.target.value >= 1) || e.target.value === '') {
+      setDate((prevDate) => {
+        return [prevDate[0], e.target.value, prevDate[2]]
+      })
+    }
+  }
+
+  function changeYear (e) {
+    if((e.target.value <= 2024 && e.target.value >= 1) || e.target.value === '') {
+      setDate((prevDate) => {
+        return [prevDate[0], prevDate[1], e.target.value]
+      })
+    }
+  }
 
 
   return (
     <>
       Day
-      DD
+      <input type={'number'} min={1} max={31} value={day} onInput={changeDay}></input>
 
       Month
-      MM
+      <input type={'number'} min={1} max={12} value={month} onInput={changeMonth}></input>
 
       Year
-      YYYY
+      <input type={'number'} min={1} max={2024} value={year} onInput={changeYear}></input>
 
       -- years
       -- months
