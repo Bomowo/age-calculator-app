927b2e1e43f5d9e05c65eb15a0b7a780339a572a
Bomowo <oeccc25@yahoo.com>
2024-06-18 18:08:04 +0300

    Add check if day is in range of 1-31

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,62 @@ import { useState } from "react"
 function App() {
   let [[bDay, bMonth, bYear], setDate] = useState([1,1,1])
   let [[diffDay, diffMonth, diffYear], setDiffDate] = useState([null, null, null])
+  let [[errorDay, errorMonth, errorYear, errorReq], setError] = useState([null, null, null, null])
 
   function changeDay (e) {
-    if((e.target.value <= 31 && e.target.value>=1)  || e.target.value === '') {
+      if(e.target.value < 1 || e.target.value > 31) {
+        console.log(e.target.value)
+        setError((prevErrors) => {
+          return ['Must be a valid day', prevErrors[1], prevErrors[2], prevErrors[3]]
+        })
+      } else {
+        setError((prevErrors) => {
+          return [null, prevErrors[1], prevErrors[2], prevErrors[3]]
+        })
+      }
+    
       setDate((prevDate) => {
+        console.log('ok!')
         return [e.target.value, prevDate[1], prevDate[2]]
       })
-    }
   }
 
   function changeMonth (e) {
-    if((e.target.value <= 12 && e.target.value >= 1) || e.target.value === '') {
       setDate((prevDate) => {
         return [prevDate[0], e.target.value, prevDate[2]]
-      })
-    }
+      })  
   }
 
   function changeYear (e) {
-    if((e.target.value <= 2024 && e.target.value >= 1) || e.target.value === '') {
       setDate((prevDate) => {
         return [prevDate[0], prevDate[1], e.target.value]
       })
-    }
   }
 
   function calculateAgeDiff () {
-    let currentDate = new Date()
-    let inputDate = new Date()
 
-    inputDate.setFullYear(bYear)
-    inputDate.setMonth(bMonth)
-    inputDate.setDate(bDay)
-    
-    let dateDiff = Math.abs(currentDate - inputDate)
 
-    let dateDiffYear = Math.floor(dateDiff / 31556952000)
-    dateDiff = dateDiff - (dateDiffYear * 31556952000)
-    let dateDiffMonth = Math.floor(dateDiff / 2629746000)
-    dateDiff = dateDiff - (dateDiffMonth * 2629746000)
-    let dateDiffDay = Math.floor(dateDiff / 86400000)
 
-    setDiffDate([dateDiffDay, dateDiffMonth, dateDiffYear])
+    if(!errorDay && !errorMonth && !errorYear && !errorReq) {
+      console.log(!errorDay && !errorMonth && !errorYear && !errorReq, [errorDay, errorMonth, errorYear, errorReq])
+      let currentDate = new Date()
+      let inputDate = new Date()
+  
+      inputDate.setFullYear(bYear)
+      inputDate.setMonth(bMonth)
+      inputDate.setDate(bDay)
+      
+      let dateDiff = Math.abs(currentDate - inputDate)
+  
+      let dateDiffYear = Math.floor(dateDiff / 31556952000)
+      dateDiff = dateDiff - (dateDiffYear * 31556952000)
+      let dateDiffMonth = Math.floor(dateDiff / 2629746000)
+      dateDiff = dateDiff - (dateDiffMonth * 2629746000)
+      let dateDiffDay = Math.floor(dateDiff / 86400000)
+  
+      setDiffDate([dateDiffDay, dateDiffMonth, dateDiffYear])
+    }
+
 
 
     // const diffTime = Math.abs(currentDate - inputDate);
@@ -56,13 +70,14 @@ function App() {
   return (
     <>
       Day
-      <input type={'number'} min={1} max={31} value={bDay} onInput={changeDay}></input>
+      <input type={'number'} value={bDay} onInput={changeDay}></input>
+      {errorDay && <p>{errorDay}</p>}
 
       Month
-      <input type={'number'} min={1} max={12} value={bMonth} onInput={changeMonth}></input>
+      <input type={'number'} value={bMonth} onInput={changeMonth}></input>
 
       Year
-      <input type={'number'} min={1} max={2024} value={bYear} onInput={changeYear}></input>
+      <input type={'number'} value={bYear} onInput={changeYear}></input>
 
 
       <button onClick={calculateAgeDiff}>Submit</button>
